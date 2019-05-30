1d7725ca23eeb22a3d3aa6eca955f3800dd14bad
meno.m <mokuya18@gmail.com>
2019-05-30 20:55:56 +0900

    雛形作り直し

---
+++ todo-list/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    sourceType: 'module',
+    parser: 'babel-eslint'
+  },
+  env: {
+    browser: true
+  },
+  extends: ['prettier', 'prettier/standard', 'plugin:vue/recommended'],
+  plugins: ['vue', 'prettier'],
+  rules: {
+    'generator-star-spacing': 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    properties: 'always',
+    ignoreDestructuring: false,
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'none',
+        singleQuote: true,
+        semi: false,
+        printWidth: 120
+      }
+    ]
+  }
+}
---
+++ todo-list/src/components/Todo.vue
@@ -0,0 +1,52 @@
+<template>
+  <div>
+    <form>
+      <div>
+        <label>タイトル</label>
+        <input v-model="todoForm.title" type="text">
+      </div>
+      <div>
+        <label>詳細</label>
+        <input v-model="todoForm.description" type="text">
+      </div>
+    </form>
+    <button v-on:click="createTodo">TODO作成</button>
+    <p>{{ todoForm }}</p>
+    <div>
+      <ul>
+        <li v-for="todo in todoList" :key="todo.title">
+          <p>{{todo.title}}</p>
+          <p>{{todo.description}}</p>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+interface TodoForm {
+  title: string;
+  description?: string;
+}
+
+@Component
+export default class TodoList extends Vue {
+  private todoForm: TodoForm = {
+    title: "",
+    description: ""
+  };
+
+  todoList: TodoForm[] = [];
+  createTodo() {
+    console.log("click");
+    const todo = Object.assign({}, this.todoForm);
+    this.todoList.push(todo);
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
